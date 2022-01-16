4a95ad5a29a93d2c2ccf8d2a6453f9637a115e0d
Karim Pazoki <k.pazoki@dorehgard.ir>
2022-01-17 02:26:22 +0330

    Update commands to create module directories

--- src/Console/GenerateModuleBlueprint.php
+++ src/Console/GenerateModuleBlueprint.php
@@ -8,4 +8,62 @@ use Illuminate\Support\Facades\File;
 class GenerateModuleBlueprint extends Command
 {
     protected $signature = 'make:module {name}';
+
+    protected $description = 'create a new module blueprint';
+
+    public function handle()
+    {
+        $module_name = $this->argument('name');
+
+        $module_directory_tree = array(
+            $module_name => [
+                "Config" => [],
+                "Console" => [
+                    "Commands" => []
+                ],
+                "Controllers" => [
+                    "Core" => [],
+                    "Services" => [],
+                ],
+                "Database" => [
+                    "Migrations" => [],
+                    "Factories" => [],
+                    "Seeders" => [],
+                ],
+                "Events" => [],
+                "Exceptions" => [],
+                "Facades" => [],
+                "Listeners" => [],
+                "Models" => [
+                    "Entities" => [],
+                    "Domains" => [],
+                    "Observers" => [],
+                    "Repositories" => [],
+                ],
+                "Providers" => [],
+                "Policies" => [],
+                "Resources" => [],
+                "Requests" => [],
+                "Routes" => [],
+                "Validators" => [],
+                "Views" => [],
+            ]
+
+        );
+
+        $path = app_path();
+        $this->makeDiretoriesTree($path, 'Modules', $module_directory_tree);
+    }
+
+    public function makeDiretoriesTree(string $path, string $directory_name, array $sub_directories)
+    {
+        $path = $path . DIRECTORY_SEPARATOR . $directory_name;
+
+        foreach ($sub_directories as $directory_name => $sub_directory) {
+            $this->makeDiretoriesTree($path, $directory_name, $sub_directory);
+        }
+        if (count($sub_directories) == 0) {
+            File::makeDirectory($path, $mode = 0755, true, true);
+        }
+    }
 }
--- src/Console/InstallModuleBlueprint.php
+++ src/Console/InstallModuleBlueprint.php
@@ -4,6 +4,7 @@ namespace Kaafochino\ModuleBlueprint\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Artisan;
 
 class InstallModuleBlueprint extends Command
 {
@@ -15,10 +16,30 @@ class InstallModuleBlueprint extends Command
     {
         $this->info('Initialize ModuleBlueprint...');
 
-        $path = app_path() . '/Modules';
+        Artisan::call('make:module User');
 
-        File::makeDirectory($path, $mode = 0755, true, true);
+        $base_directories = array(
+            'Core' => [],
+            'Helpers' => [],
+            'Libraries' => [],
+        );
+
+        $path = app_path();
+        foreach ($base_directories as $directory => $sub_directories) {
+            $this->makeDir($path, $directory, $sub_directories);
+        }
 
         $this->info('ModuleBlueprint Initialized');
+    }
+    public function makeDir(string $path, string $directory_name, array $sub_directories)
+    {
+        $path = $path . DIRECTORY_SEPARATOR . $directory_name;
+
+        foreach ($sub_directories as $directory_name => $sub_directory) {
+            $this->makeDir($path, $directory_name, $sub_directory);
+        }
+        if (count($sub_directories) == 0) {
+            File::makeDirectory($path, $mode = 0755, true, true);
+        }
     }
 }
